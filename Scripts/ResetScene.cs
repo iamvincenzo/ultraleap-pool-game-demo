84a0121097266dd6e6b28f88364cf39477a5ab64
vince <43069944+iamvincenzo@users.noreply.github.com>
2023-03-24 01:58:14 +0100

    update script

--- Scripts/ResetScene.cs
+++ Scripts/ResetScene.cs
@@ -4,26 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ResetScene : MonoBehaviour
 {
-    public GameObject objectToSpawn;
-    public Transform spawnPoint;
-    public GameObject centralTableLimits;
     //Reset current scene
     public void ResetMainScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-
-    public void SpawnRandomObject()
-    {
-        var size = GameObject.Find("GrassTableField7").GetComponent<Collider>().bounds.size;
-
-        Vector3 center = new Vector3(centralTableLimits.transform.position.x, 1, centralTableLimits.transform.position.z);
-
-        var px = Random.Range(-size.x / 2, size.x / 2);
-        var pz = Random.Range(-size.z / 2, size.z / 2);
-
-        Vector3 pos = center + new Vector3(px, spawnPoint.position.y, pz);
-        Instantiate(objectToSpawn.transform, pos, Quaternion.identity, spawnPoint);
     }
 }
